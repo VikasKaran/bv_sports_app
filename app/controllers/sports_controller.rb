85a4bba283bef17ebf5632c1eaab23a590fbde6e
VikasKaran <vikas.karan9@gmail.com>
2023-06-07 17:01:47 +0100

    Added Error Handeling as per api client in sports controller

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -2,7 +2,12 @@ class SportsController < ApplicationController
 
   def index
     sports_data = sports_client.sports_data
-    @sports = sports_data.sort_by { |sport| sport['pos'] } || []
+    if @sports.is_a?(Hash) && sports_data.key?(:error)
+      @error_code = sports_data[:error][:code]
+      @error_message = sports_data[:error][:message]
+    else
+      @sports = sports_data.sort_by { |sport| sport['pos'] } || []
+    end    
 
     respond_to do |format|
       format.html
