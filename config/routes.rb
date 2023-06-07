eb1bee18be7141631a0bd5079430ffefb4618184
VikasKaran <vikas.karan9@gmail.com>
2023-06-07 14:56:05 +0100

    Created controller Sports with Index and Events with index and show method

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'events/index'
+  get 'events/show'
+  get 'sports/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
