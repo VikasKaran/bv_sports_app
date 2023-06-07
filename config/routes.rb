4177546991ba77113705609bb3b0ca3d2cc6e46e
VikasKaran <vikas.karan9@gmail.com>
2023-06-07 17:17:28 +0100

    updated sports resource to resources to correct error

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "sports#index"
-  resource :sports, only: [:index] do
+  resources :sports, only: [:index] do
     resources :events, only: [:index, :show]
   end
 end
