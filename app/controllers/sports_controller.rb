67af3eb7ce064870f665e4c5dfe55cd1a32c592e
VikasKaran <vikas.karan9@gmail.com>
2023-06-07 16:27:11 +0100

    Sports controller with index method and api call from services.

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -1,4 +1,18 @@
 class SportsController < ApplicationController
+
   def index
+    sports_data = sports_client.sports_data
+    @sports = sports_data.sort_by { |sport| sport['pos'] } || []
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @sports.to_json}
+    end
+  end
+
+  private
+
+  def sports_client
+    @sports_client ||= ApiClients::SportsEventsClient.new
   end
 end
